9428493c4969be1f62fca1768e156180e85ac05b
chaim baror <chbaror@gmail.com>
2021-04-11 12:27:51 +0300

    first commit

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const data = [
-    { id: 1, name: "דסקטופ", value: "דסקטופ" },
-    { id: 2, name: "מגה פלאזמה וידאו", value: "" },
-    { id: 3, name: "מגה פלאזמה", value: "" },
-    { id: 4, name: "פלאזמה", value: "" },
-    { id: 5, name: "אינרבוקס", value: "אינרבוקס" },
-    { id: 6, name: "מגה מגדל", value: "" },
-    { id: 7, name: "אוזן מגדל", value: "" },
+    { id: 1, name: "דסקטופ", value: "דסקטופ", img: "https://www.israelhayom.co.il/sites/default/files/styles/566x349/public/images/articles/2020/12/16/16081297527953_b.jpeg" },
+    { id: 2, name: "מגה פלאזמה וידאו", value: "",img:"https://unfurlmedia.com/wp-content/uploads/2019/03/DIGITAL-MARKETING-TOOLS.jpg" },
+    { id: 3, name: "מגה פלאזמה", value: "" ,img:"https://miro.medium.com/max/2400/1*NLoUfqTzlgN3BDGnD7V1Wg.jpeg"},
+    { id: 4, name: "פלאזמה", value: "" ,img:"https://miro.medium.com/max/875/1*2kIEtclEK9k8wwWbNsZL8Q.jpeg"},
+    { id: 5, name: "אינרבוקס", value: "אינרבוקס" ,img:".com/images?q=tbn:ANd9GcTwu30hF_Pa44a1r_sIka6CNk73PajBlipUWVO1RTDQYYqmj22um2SZwp3mvfGJg26Fo8k" },
+    { id: 6, name: "מגה מגדל", value: "" ,img:"https://miro.medium.com/max/875/1*2kIEtclEK9k8wwWbNsZL8Q.jpeg" },
+    { id: 7, name: "אוזן מגדל", value: "" , img: "https://www.israelhayom.co.il/sites/default/files/styles/566x349/public/images/articles/2020/12/16/16081297527953_b.jpeg"  },
     { id: 8, name: "קוביה בעמוד הבית", value: "" },
-    { id: 9, name: "פרירול", value: "פרירול" },
+    { id: 9, name: "פרירול", value: "פרירול", img: "https://www.israelhayom.co.il/sites/default/files/styles/566x349/public/images/articles/2020/12/16/16081297527953_b.jpeg"  },
 ]
 
-let itemDesktop = "";
-let itemMobile = "";
+let itemDesktop = { name: "", img: "", value: "" };
+let itemMobile = { name: "", img: "", value: "" };
 
 
 
@@ -42,8 +42,7 @@ function menuItemsSelect() {
 
 
 function clickedDesktop(eventId) {
-    itemDesktop = data.find(x => x.id === eventId).name
-    console.log("clicked", itemDesktop);
+    itemDesktop = data.find(x => x.id === eventId)
     showDesktopHTML()
 
     //this is jquery  active
@@ -56,7 +55,7 @@ function clickedDesktop(eventId) {
 }
 
 function clickedOfMobile(eventId) {
-    itemMobile = data.find(x => x.id === eventId).name
+    itemMobile = data.find(x => x.id === eventId)
     console.log("clickedOfMobile", itemMobile);
     showMobilepHTML()
 
@@ -70,23 +69,24 @@ function clickedOfMobile(eventId) {
 }
 
 
-function showItems() {
-    return `<div class="showItems">${itemDesktop}</div>`
-}
-function showItemsMobile() {
-    return `<div class="showItemsMobile">${itemMobile}</div>`
-}
-
 function showDesktopHTML() {
-    document.querySelector('.desktop').innerHTML = `<div class="desktop">
+    document.querySelector('.desktop').innerHTML = `<div id="desktop">
      <img src="assets/desktop-desktop.png" class="rounded" alt="">
-     ${showItems()}
+     <div class="showItems row">
+     <div class="col-4">${itemDesktop.name}</div>
+     <div  class="col-8"> <img src=${itemDesktop.img} class="rounded" alt="..."></div>
+    
+     </div>
+
      </div>`
 }
 function showMobilepHTML() {
-    document.querySelector('.mobile').innerHTML = `<div class="mobile"> 
+    document.querySelector('.mobile').innerHTML = `<div id="mobile"> 
      <img src="assets/phone-desctop.png" class="rounded" alt="" >
-     ${showItemsMobile()}
+     <div class="showItemsMobile">
+     <div >${itemMobile.name}</div>
+     <div  ><img src=${itemMobile.img} class="rounded" alt="..."></div>
+
      </div>`
 
 }
